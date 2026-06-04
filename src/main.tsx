a6abbeb91d1294e0bd6f0e3b7f5e8b4ae028ef3f
huynbq <huy.nbq@bioturing.com>
2026-06-04 15:53:51 +0700

    feat: setup query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 import App from "./App.tsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
