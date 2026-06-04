bf44a95a77d0dc1a56e703f0a082cd8e16ea61b8
huynbq <huy.nbq@bioturing.com>
2026-06-04 17:20:31 +0700

    feat: add tooltip provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import { Route, Routes } from "react-router";
 import { AuthProvider } from "./auth/AuthContext";
 import { AuthGate } from "./auth/AuthGate";
 import Loading from "./components/Loading";
-import MainLayout from "./layouts/MainLayout";
 import routes from "./pages/routes";
+import { TooltipProvider } from "./components/ui/tooltip";
+import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
     <AuthProvider>
       <AuthGate>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route element={<MainLayout />}>
-              {routes.map((route) => (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  element={<route.component />}
-                />
-              ))}
-            </Route>
-          </Routes>
-        </Suspense>
+        <TooltipProvider>
+            <Suspense fallback={<Loading />}>
+              <Routes>
+                <Route element={<MainLayout />}>
+                  {routes.map((route) => (
+                    <Route
+                      key={route.path}
+                      path={route.path}
+                      element={<route.component />}
+                    />
+                  ))}
+                </Route>
+              </Routes>
+            </Suspense>
+        </TooltipProvider>
       </AuthGate>
     </AuthProvider>
   );
